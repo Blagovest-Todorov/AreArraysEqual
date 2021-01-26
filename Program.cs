af326f2fa06fc797647dcb07e3e5424b60ee3556
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-26 10:58:56 +0200

    add solution file Program.cs

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrOne = Console.ReadLine()
+                           .Split()
+                           .Select(int.Parse)
+                           .ToArray();
+
+            int[] arrTwo = Console.ReadLine()
+                           .Split()
+                           .Select(int.Parse)
+                           .ToArray();
+
+            bool areEqual = true;
+            int sum = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i < arrOne.Length; i++)
+            {
+                if (arrOne[i] != arrTwo[i])
+                {                    
+                    areEqual = false;
+                        // indexNum = i;                       
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index"); 
+                    break;
+                }                    
+                else //   (arrOne[i] == arrTwo[i])
+                {
+                    sum += arrOne[i];
+                }
+
+                result = ($"Arrays are identical. Sum: {sum}");
+            }
+
+            if (result != string.Empty)
+            {
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
